3ded8c60280fd2ecedbcc1d9c2cce07d5e7f44ef
INORIA <inoria7044@gmail.com>
2015-07-23 00:33:06 +0900

    Now run.sh can be executed everywhere in this git

--- utils/run.sh
+++ utils/run.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+cd $(git rev-parse --show-toplevel)
+
 DATA_NAME=bind9-data
 
 # Create bind9-data container
-docker run -d -v /etc/bind --name $DATA_NAME inoria/bind9
+cd Dockerfiles/bind9-data
+cp ../../data/* ./data/
+docker build -t inoria/bind9-data .
+# docker run -d -v /etc/bind --name $DATA_NAME inoria/bind9
+
 
