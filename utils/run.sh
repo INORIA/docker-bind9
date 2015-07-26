53f209d9d51d1608c4e6e209559f1ce8b6cdd0de
INORIA <inoria7044@gmail.com>
2015-07-26 23:19:25 +0900

    Change data cp procedure

--- utils/run.sh
+++ utils/run.sh
@@ -11,8 +11,8 @@ cp ../../data/* ./data/
 docker build -t inoria/bind9-data .
 docker run -d --name $DATA_NAME inoria/bind9-data /bin/bash
 # clean up
-git checkout .
-git clean -df .
+git checkout data
+git clean -df data
 
 # Create bind9 server container
 docker run -d --volumes-from bind9-data --name $NAME inoria/bind9 /sbin/my_init
