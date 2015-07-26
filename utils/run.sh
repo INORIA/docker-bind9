71ee10e2ed8f9b34ded6d9c359c96a04115f7750
INORIA <inoria7044@gmail.com>
2015-07-26 23:58:43 +0900

    Fix cp option

--- utils/run.sh
+++ utils/run.sh
@@ -7,12 +7,12 @@ DATA_NAME=bind9-data
 
 # Create bind9 data volume container
 cd Dockerfiles/bind9-data
-cp ../../data/* ./data/
+cp -r ../../data/* ./data/
 docker build -t inoria/bind9-data .
 docker run -d --name $DATA_NAME inoria/bind9-data /bin/bash
 # clean up
-git checkout data
-git clean -df data
+# git checkout data
+# git clean -df data
 
 # Create bind9 server container
 docker run -d --volumes-from bind9-data --name $NAME inoria/bind9 /sbin/my_init
