8b3e0fc33825a8ad77243b0babd7c190f27df72c
INORIA <inoria7044@gmail.com>
2015-07-29 13:46:31 +0900

    Run if images exist

--- utils/run.sh
+++ utils/run.sh
@@ -5,6 +5,20 @@ cd $(git rev-parse --show-toplevel)
 NAME=bind9
 DATA_NAME=bind9-data
 
+
+# if required images exists just exec run
+if docker images | grep -q 'inoria/bind9'; then
+	if docker images | grep -q 'inoria/bind9-data'; then
+		# remove existent 'bind9' bind9-data' container
+		docker ps -a | grep 'bind9' | awk '{print $1}' | xargs --no-run-if-empty -I % bash -c 'docker stop %; docker rm %'
+
+		docker run -it -d --name $DATA_NAME inoria/bind9-data /bin/bash
+		docker run -d -p 53:53 -p 53:53/udp --volumes-from bind9-data --name $NAME inoria/bind9 /sbin/my_init
+		exit 1
+	fi
+fi
+
+
 # Create bind9 data volume container
 cd Dockerfiles/bind9-data
 rm -r ./data/*
